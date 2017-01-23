e0b45a6ccfee8e13d7402d7d7b5a134ab3b4654e
Rizky Ario <rizkyario@yahoo.com>
2017-01-23 15:55:08 +0700

    Init Project

---
+++ Assets/FadeAnimation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class FadeAnimation : MonoBehaviour {
+
+	public Animation anim;
+	void Start() {
+		anim = GetComponent<Animation>();
+	}
+	void Update() {
+		if (Mathf.Abs (Input.GetAxis ("Vertical")) > 0.1F) {
+			anim.CrossFade ("walk1");
+			Debug.Log ("Run");
+		} else {
+			anim.CrossFade ("run");
+			Debug.Log ("Walk");
+		}
+	}
+}
